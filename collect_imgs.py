c2f53d13d5dc0b0ff8cc79789e913898a60a8f2e
jaspreetkaur1010 <138621287+jaspreetkaur1010@users.noreply.github.com>
2024-05-04 17:13:18 +0530

    Add files via upload

---
+++ collect_imgs.py
@@ -0,0 +1,55 @@
+import os
+import cv2
+import time
+
+# Add a delay before starting data collection
+time.sleep(2)  # Delay for 2 seconds
+
+DATA_DIR = './data'
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
+
+number_of_classes = 11
+dataset_size = 100
+
+cap = cv2.VideoCapture(0)
+for j in range(number_of_classes):
+    if not os.path.exists(os.path.join(DATA_DIR, str(j))):
+        os.makedirs(os.path.join(DATA_DIR, str(j)))
+
+    print('Collecting data for class {}'.format(j))
+
+    done = False
+    while True:
+        ret, frame = cap.read()
+        if not ret:  # Check if frame is valid
+            print("Error: Failed to capture frame")
+            break
+
+        # Mirror the frame horizontally
+        frame = cv2.flip(frame, 1)
+
+        cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
+                    cv2.LINE_AA)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(25) == ord('q'):
+            break
+
+    counter = 0
+    while counter < dataset_size:
+        ret, frame = cap.read()
+        if not ret:  # Check if frame is valid
+            print("Error: Failed to capture frame")
+            break
+
+        # Mirror the frame horizontally
+        frame = cv2.flip(frame, 1)
+
+        cv2.imshow('frame', frame)
+        cv2.waitKey(25)
+        cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame)
+
+        counter += 1
+
+cap.release()
+cv2.destroyAllWindows()
